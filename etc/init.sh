4a7a8d32a9e5411a6fd66855b7d1f9e5bbb1876d
Alexander Prymak <primak.aa@gmail.com>
2021-01-03 04:37:05 +0300

    Added init.sh and test index.html

---
+++ etc/init.sh
@@ -0,0 +1,7 @@
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
+#sudo /etc/init.d/mysql start
+
