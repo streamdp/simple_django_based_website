26c2e36ab56591a5e030464e73f209f49e69c08a
Alexander Prymak <primak.aa@gmail.com>
2021-01-04 01:39:39 +0000

    add test django project

--- etc/init.sh
+++ etc/init.sh
@@ -1,7 +1,10 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/default  /etc/nginx/sites-enabled/
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo rm -rf /etc/gunicorn.d/test_conf.py
+sudo rm -rf /etc/gunicorn.d/django_conf.py
+sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test_conf.py
+sudo ln -sf /home/box/web/etc/django_conf.py /etc/gunicorn.d/django_conf.py
 sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
 
