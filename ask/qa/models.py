4f26704adae9f5fdcef632e990ea48d8078fbe59
Alexander Prymak <primak.aa@gmail.com>
2021-01-04 23:54:16 +0000

    aaa, i need make backup :)

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -7,14 +7,14 @@ class Question(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
-    author = models.ForeignKey(User, null=False, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name='likes_set')
 
 class Answer(models.Model):
     text = models.TextField()                                                   
     added_at = models.DateTimeField(auto_now_add=True)
-    question = models.ForeignKey(Question, null=False, on_delete=models.SET_NULL) 
-    author = models.ForeignKey(User, null=False, on_delete=models.SET_NULL)     
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) 
+    author = models.ForeignKey(User, on_delete=models.CASCADE)     
 
 class QuestionManager(models.Manager):
     def new(self):
