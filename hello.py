e37831b58efc81b8f0eca7892a0e7398d7f179b7
Alexander Prymak <primak.aa@gmail.com>
2021-01-04 00:12:38 +0000

    fixed gunicorn.conf, fixed hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,9 @@
-def wsgi_app(environ, start_response):
+def application(environ, start_response):
     # бизнес-логика
     status = '200 OK'
     headers = [
         ('Content-Type', 'text/plain')
     ]
-    body = 'Hello, world!'
+    body = [(arg+'\n').encode('ascii') for arg in environ['QUERY_STRING'].split('&')]
     start_response(status, headers )
-    print(environ['QUERY_STRING'])
-    return [ body ]
+    return body
