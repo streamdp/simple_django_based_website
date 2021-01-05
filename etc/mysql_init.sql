4c33655856a1fb3a09fe88dd93f97fe1b8028186
Alexander Prymak <primak.aa@gmail.com>
2021-01-05 00:43:41 +0000

    make some changes in mysql_init.slq

--- etc/mysql_init.sql
+++ etc/mysql_init.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE django_db;
+CREATE DATABASE IF NOT EXISTS django_db CHARACTER SET utf8;
 CREATE USER 'django_user'@'localhost' IDENTIFIED BY 'django_password';
 GRANT ALL PRIVILEGES ON django_db . * TO 'django_user'@'localhost';
 FLUSH PRIVILEGES;
