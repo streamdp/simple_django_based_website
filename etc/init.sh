eed38bf8c8e2c4b48388ed010a3909da626c20d4
Alexander Prymak <primak.aa@gmail.com>
2021-01-05 14:32:52 +0300

    fix template questions.html & some fix init.sh

--- etc/init.sh
+++ etc/init.sh
@@ -1,12 +1,13 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/default  /etc/nginx/sites-enabled/
+sudo ln -sf $PWD/default  /etc/nginx/sites-enabled/
 sudo /etc/init.d/nginx restart
+sudo mkdir -p /etc/gunicorn.d/
 sudo rm -rf /etc/gunicorn.d/test_conf.py
 sudo rm -rf /etc/gunicorn.d/django_conf.py
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test_conf.py
-sudo ln -sf /home/box/web/etc/django_conf.py /etc/gunicorn.d/django_conf.py
+sudo ln -sf $PWD/gunicorn.conf   /etc/gunicorn.d/test_conf.py
+sudo ln -sf $PWD/django_conf.py /etc/gunicorn.d/django_conf.py
 sudo mkdir -p /etc/mysql/mysql.conf.d/
-sudo ln -sf /home/box/web/etc/django.cnf /etc/mysql/mysql.conf.d/
+sudo ln -sf $PWD/django.cnf /etc/mysql/mysql.conf.d/
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
-/home/box/web/etc/setup_git.sh
+$PWD/setup_git.sh
