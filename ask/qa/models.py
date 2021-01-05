afbb1bcbc143862863a30bb248d475f43435b0fc
Alexander Prymak <primak.aa@gmail.com>
2021-01-05 03:40:59 +0000

    add some test data and back-up it

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
+from django.db import models
 # Create your models here.
+
 
 class QuestionManager(models.Manager):                                          
     def new(self):                                                                      
@@ -14,12 +16,12 @@ class Question(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    likes = models.ManyToManyField(User, related_name='likes_set')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='likes_set')
     objects = QuestionManager()
 
 class Answer(models.Model):
     text = models.TextField()                                                   
     added_at = models.DateTimeField(auto_now_add=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE) 
-    author = models.ForeignKey(User, on_delete=models.CASCADE)     
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)     
